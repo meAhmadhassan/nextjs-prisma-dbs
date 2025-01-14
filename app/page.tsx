ef48645eff19193d85fa6c21edca849b539d13dd
Your Name <meahmadhassan@users.noreply.github.com>
2025-01-14 18:12:58 +0500

    postgresql

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
 import { db } from "@/lib/db";
 
 export default async function Home() {
-  const tasks = await db.task.findMany();
+  // const tasks = await db.task.findMany();
+
+  // filtered tasks 
+  const tasks = await db.task.findMany({
+    where: {
+      title: { contains : "task" }
+    }
+  })
 
   return (
     <div>
       <h2 className="text-center mt-20">Home Page</h2>
+      <p>Tasks List</p>
       {tasks.map((task) => (
         <div key={task.id}>{task.id} - {task.title}</div>
       ))}
